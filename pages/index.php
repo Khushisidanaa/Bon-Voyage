4e663ccb88f0f591b05319ba3210865c88a29831
Khushi Sidana <khushisidana@Khushis-MacBook-Air-3.local>
2023-11-23 22:57:03 -0600

    first commit

---
+++ pages/index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Bon Voyage</title>
+
+    </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+</head>
+
+<body>
+<?php include('function.php'); ?>
+
+<?php include('top.php'); ?>
+<!--/sticky-->
+<?php include('slider.php'); ?>
+
+<!-- the specality page -->
+
+
+<div class="name">
+<h4>
+Plan your Trip 
+</h4>
+
+<h2>
+BonVoyage Tours and Travels 
+</h2>
+
+</div>
+
+<div id ='service' class="special">
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<div class="single-service-box">
+						<div class="service-img">
+							<img src="images/service/s1.png" alt="service-icon" />
+						</div>
+						<!--/.service-img-->
+						<div class="service-content">
+							<h2>
+								<a href="#">
+									Amazing tour packages
+								</a>
+							</h2>
+							<p>Cost effective and customised packages made just for you.</p>
+						</div>
+						<!--/.service-content-->
+					</div>
+</div>
+
+<div class="col-md-4">
+<div class="single-service-box">
+						<div class="service-img">
+							<img src="images/service/s2.png" alt="service-icon" />
+						</div>
+						<!--/.service-img-->
+						<div class="service-content">
+							<h2>
+								<a href="#">
+									Book Top Hotels
+								</a>
+							</h2>
+							<p>Best Hotels with the best views <br> at the best prices.</p>
+						</div>
+						<!--/.service-content-->
+					</div>
+</div>
+
+<div class="col-md-4">
+<div class="single-service-box">
+						<div class="service-img">
+							<img src="images/service/s3.png" alt="service-icon" />
+						</div>
+						<!--/.service-img-->
+						<div class="service-content">
+							<h2>
+								<a href="#">
+									Online Flight Booking
+								</a>
+							</h2>
+							<p>Cost effective and customised packages made just for you..</p>
+						</div>
+						<!--/.service-content-->
+					</div>
+
+                    
+</div>
+
+</div>
+</div>
+</div>
+
+
+<!- about us-->
+<div id='about-us' class="about-us">
+<h2>
+About us </h2>
+
+
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<div class="card">
+<div class="card-body">
+<h4 class="card-title">
+Mission </h4>
+<p class="card-text">
+Our goal is to become a trusted client advisor, a company that can be counted on for insight; a commitment to work with you in developing strategic travel plans and tactics designed to give you the best value addition and service , which can be availed of in the travel Industry.
+</p>
+<a href="#" class="class-link">Read More</a></div>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card">
+<div class="card-body">
+<h4 class="card-title">
+Vission </h4>
+<p class="card-text abc">
+Offer a personalized service of quality and reliability through careful attention, properly training our personnel, designing unique journeys at affordable prices, and exceeding the expectations of our clients. 
+
+</p>
+<a href="#" class="class-link">Read More</a></div>
+</div>
+</div><div class="col-md-4">
+<div class="card">
+<div class="card-body">
+<h4 class="card-title">
+Safety </h4>
+<p class="card-text abc">
+Our Extensive COVID-19 Safety Guidelines ensure your well-being is looked after during your travels and that the only thing you have to think is about enjoying the best vacations of your life with us. Well take care of the rest
+
+</p>
+<a href="#" class="class-link">Read More</a></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div id='photo-corner' class="photo-corner">
+<h2>
+Photo Corner 
+</h2>
+
+<div class="container">
+<div class="row">
+    <div class="col-md-6 d-flex">
+    		<img class='img-fluid' src="images/gt2.jpg" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Srinagar
+											</a>
+											<p><span>12 tours</span><br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+								
+        
+    </div>
+
+    <div class="col-md-6 d-flex">
+       
+    <img class='img-fluid' src="images/Kerala3.jpg" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Kerala
+											</a>
+											<p><span>12 tours</span> <br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+      
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-md-4 d-flex">
+     
+    <img class='img-fluid' src="images/ooty2new.jpg" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Ooty
+											</a>
+											<p><span>12 tours</span> <br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+      
+    </div>
+    <div class="col-md-4 d-flex">
+      
+    <img class='img-fluid' src="images/Srinagar3.jpg" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Mussoorie
+											</a>
+											<p><span>12 tours</span> <br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+       
+    </div>
+
+    <div class="col-md-4 d-flex">
+ 
+    <img class='img-fluid' src="images/MANALI1.png" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Manali
+											</a>
+											<p><span>12 tours</span> <br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-6 d-flex">
+       
+    <img class='img-fluid' src="images/LADAKH 1.png" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Ladakh
+											</a>
+											<p><span>12 tours</span> <br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+    
+    </div>
+
+    <div class="col-md-6 d-flex">
+
+    <img class='img-fluid' src="images/vd1.jpg" alt="portfolio image" />
+										<div class="item-title">
+											<a href="#">
+												Vaishno Devi
+											</a>
+											<p><span>12 tours</span><br><span>9 places</span></p>
+										</div> <!-- /.item-title-->
+     
+    </div>
+</div>
+</div>
+</div>
+
+
+
+</div>
+
+
+<br><br><br><br><br>
+
+</body>
+</html>
---
+++ pages/top.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-inverse fixed-top">
+    <ul class="navbar-nav">
+    <li class="nav-item active ">
+        <a class="nav-link apple" href="#">&nbsp&nbspBonVoyage&nbsp&nbsp</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php#about-us" onclick="javascript:window.open('index.php#about-us','_self')">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php#photo-corner" onclick="javascript:window.open('index.php#photo-corner','_self')">Photo Corner</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Categories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Admin Page</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+</body>
+</html>
+
+
